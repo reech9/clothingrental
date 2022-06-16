59119298cf8909f824e3a75d0a8efea0865f7f97
reech9 <reech.p8@gmail.com>
2022-06-16 13:58:34 +0545

    complete styling and functionalities of login

--- lib/Screens/loginscreen.dart
+++ lib/Screens/loginscreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:motion_toast/motion_toast.dart';
+
+import '../repository/loginrepository.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,6 +11,29 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController(text: 'kiran');
+  final _passwordController = TextEditingController(text: 'kiran');
+
+  _loginUser() async {
+    bool isLogin = await UserRepository()
+        .login(_emailController.text, _passwordController.text);
+    if (isLogin) {
+      _showMessage(true);
+    } else {
+      _showMessage(false);
+    }
+  }
+
+  _showMessage(bool isLogin) {
+    if (isLogin) {
+      Navigator.pushNamed(context, '/dashboard');
+    } else {
+      MotionToast.error(description: const Text('Invalid username or password'))
+          .show(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final logo = Hero(
@@ -21,25 +47,39 @@ class _LoginScreenState extends State<LoginScreen> {
         ));
 
     final email = TextFormField(
+      controller: _emailController,
       keyboardType: TextInputType.emailAddress,
       autofocus: false,
-      initialValue: 'kiran123@gmail.com',
+      // initialValue: 'kiran123@gmail.com',
       decoration: InputDecoration(
           hintText: 'Email',
           contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter username';
+        }
+        return null;
+      },
     );
 
     final password = TextFormField(
+      controller: _passwordController,
       autofocus: false,
-      initialValue: 'some password',
+      // initialValue: 'some password',
       obscureText: true,
       decoration: InputDecoration(
         hintText: 'Password',
         contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter password';
+        }
+        return null;
+      },
     );
 
     final loginButton = Padding(
